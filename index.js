c3e4436b98395c6244533c45950c378fab5b60a2
Gabrielle Carvalho <88461057+GabrielleCGNeves@users.noreply.github.com>
2023-04-10 16:16:34 -0300

    :sparkles: created index

---
+++ index.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html data-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+</head>
+<body>
+    <main class="container">
+        <div class="grid">
+            <h2>Desenvolvido por</h2>
+        </div>
+
+        <div class="grid">
+            <article data-theme="light">
+                <img src="./img/img.png" alt="">
+                <h3>Erick Sousa Santos</h3>
+                <p></p>
+            </article>
+            <article data-theme="light">
+                <h3>Gabrielle Carvalho Gonçalves das Neves</h3>
+            </article>
+            <article data-theme="light">
+                <h3>Guilherme Soares de Sousa</h3>
+            </article>
+        </div>
+    </main>
+</body>
+</html>
