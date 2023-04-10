6b5346a7308c73ca4a01ade25d4080014c258ea0
Gabrielle Carvalho <88461057+GabrielleCGNeves@users.noreply.github.com>
2023-04-10 16:59:42 -0300

    :lipsitck:  updated card structure

--- index.js
+++ index.js
@@ -9,21 +9,37 @@
 </head>
 <body>
     <main class="container">
+        <nav>
+            <ul>
+              <li><a href="#" class="secondary">…</a></li>
+            </ul>
+            <ul>
+              <li><h1><strong>Valida JS</strong></h1></li>
+            </ul>
+            <ul>
+              <li><a href="#" class="secondary">…</a></li>
+            </ul>
+          </nav>
         <div class="grid">
             <h2>Desenvolvido por</h2>
         </div>
 
         <div class="grid">
             <article data-theme="light">
-                <img src="./img/img.png" alt="">
-                <h3>Erick Sousa Santos</h3>
-                <p></p>
+                <img src="https://files.catbox.moe/0mxbni.png" alt="">
+                <h4>Erick Sousa Santos</h4>
+                <p>Estudante de Desenvolvimento de Software Multiplataforma na FATEC Zona Sul.</p>
+                <a href="http://ericksantos12.neocities.org">Site</a>
             </article>
             <article data-theme="light">
-                <h3>Gabrielle Carvalho Gonçalves das Neves</h3>
+                <h4>Gabrielle Carvalho Gonçalves das Neves</h4>
+                <p>Estudante de Desenvolvimento de Software Multiplataforma na FATEC Zona Sul.</p>
+                <a href="https://gabriellecgneves.neocities.org/">Site</a>
             </article>
             <article data-theme="light">
-                <h3>Guilherme Soares de Sousa</h3>
+                <h4>Guilherme Soares de Sousa</h4>
+                <p>Estudante de Desenvolvimento de Software Multiplataforma na FATEC Zona Sul.</p>
+                <p>Wesite: </p>
             </article>
         </div>
     </main>
